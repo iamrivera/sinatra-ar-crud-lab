6afc2b1d106dfcd6a6f24db07ea3efda2854c171
iamrivera <a3m3rivera@gmail.com>
2021-01-20 00:10:58 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  
   end
+
+  get '/articles' do 
+    @article = Article.all 
+    erb :index
+  end
+
+  get '/articles/new' do 
+    
+    erb :new
+  end
+
+  post '/articles' do 
+   @article = Article.create(params)
+   id = @article.id
+    redirect "/articles/#{id}"    
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params["id"])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do 
+    @article = Article.find(params["id"])
+    erb :edit
+  end
+
+  patch '/articles/:id'do 
+    # binding.pry
+    @article = Article.find(params["id"])
+    id = @article.id
+    @article.title = params["title"]
+    @article.content = params["content"]
+    @article.save #try again to get .update to work
+    redirect "/articles/#{id}"
+  end
+  
+  delete '/articles/:id' do
+    @article = Article.find(params["id"])
+    @article.destroy
+    redirect '/articles'
+  end
+
 end
